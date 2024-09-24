9e2f8c6d2e113e02f0e315de22a2a5be542941ec
jefreysi20 <jefreysi20@gmail.com>
2024-09-23 19:07:22 -0500

    feat: add sidebar menu for navigation

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+import { ProfileSettingsComponent } from '../profile-settings/profile-settings.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, ProfileSettingsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
